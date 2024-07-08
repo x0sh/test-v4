f963146160e4b783035b338879db4ac86ed9caa1
Alex <testpy739@gmail.com>
2024-07-08 14:51:29 +0200

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,10 @@
 // just test
+
+class Test {
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+    }
+}
